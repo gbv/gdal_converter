fec1eb4d0e029ab89b72bc2d45dbbf8e06d9224c
chris-jan-trapp <christian.trapp@gbv.de>
2021-05-28 13:20:07 +0200

    Added GML and WKT conversion, also requests, which might be totally useless in this context.

--- converter.py
+++ converter.py
@@ -3,12 +3,29 @@ from osgeo import ogr
 app = Flask(__name__)
 
 
+@app.route("/gml/")
+def to_gml():
+    geojson = request.get_json()
+    geometry = ogr.CreateGeometryFromJson(geojson)
+    if geometry:
+        return geometry.ExportToGML()
+
+
 @app.route("/kml/")
 def to_kml():
     geojson = request.get_json()
     geometry = ogr.CreateGeometryFromJson(geojson)
     if geometry:
-        return geometry.ExportToKml()
+        return geometry.ExportToKML()
+
+
+@app.route("/wkt/")
+def to_gml():
+    geojson = request.get_json()
+    geometry = ogr.CreateGeometryFromJson(geojson)
+    if geometry:
+        return geometry.ExportToWKT()
+
 
 
 if __name__ == "__main__":
