5cb43748920360f1d40e6f39a333b212f1f9d04e
chris-jan-trapp <christian.trapp@gbv.de>
2021-05-28 14:22:26 +0200

    Stringified json for GML.

--- converter.py
+++ converter.py
@@ -1,30 +1,35 @@
 from flask import Flask, request, render_template
 from osgeo import ogr
+import json
 app = Flask(__name__)
 
 
 @app.route("/gml/")
 def to_gml():
-    geojson = request.get_json()
+    geojson = json.dumps(request.get_json())
     geometry = ogr.CreateGeometryFromJson(geojson)
-    if geometry:
-        return geometry.ExportToGML()
+    if geometry is None:
+        return
+    return geometry.ExportToGML()
 
 
 @app.route("/kml/")
 def to_kml():
     geojson = request.get_json()
     geometry = ogr.CreateGeometryFromJson(geojson)
-    if geometry:
-        return geometry.ExportToKML()
+    if geometry is None:
+        return
+    return geometry.ExportToKML()
 
 
 @app.route("/wkt/")
 def to_wkt():
     geojson = request.get_json()
     geometry = ogr.CreateGeometryFromJson(geojson)
-    if geometry:
-        return geometry.ExportToWKT()
+    if geometry is None:
+        return
+
+    return geometry.ExportToWKT()
 
 
 if __name__ == "__main__":
