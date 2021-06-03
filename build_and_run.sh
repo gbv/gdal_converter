ba84774d7860ba7a5262edb630effab2481fdad0
chris-jan-trapp <christian.trapp@gbv.de>
2021-06-03 15:36:05 +0200

    Minor additions to the build script.

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo docker stop converter
+sudo docker container rm converter
 sudo docker build --no-cache -t converter .
 sudo docker run -d -p 5000:5000 --name converter converter
