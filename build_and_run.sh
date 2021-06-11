ad080b02f77659027d88103d9bce0d54dae6290a
chris-jan-trapp <christian.trapp@gbv.de>
2021-06-11 09:20:53 +0200

    Added network configuration to build sript.

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,3 +4,4 @@ sudo docker stop converter
 sudo docker container rm converter
 sudo docker build --no-cache -t converter .
 sudo docker run -d -p 5000:5000 --name converter converter
+sudo docker network connect easydb_default converter
