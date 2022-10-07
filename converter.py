ad01e699bdeed4eed8e6b3f335e35c964360de21
chris-jan-trapp <christian.trapp@gbv.de>
2022-10-07 12:37:40 +0200

    Added .shp -> geojson capability.

--- converter.py
+++ converter.py
@@ -1,5 +1,13 @@
+from turtle import st
 from flask import Flask, request, render_template
-from osgeo import ogr
+from osgeo import ogr, gdal
+
+from tempfile import mkdtemp
+from zipfile import ZipFile
+from os import listdir
+from glob import glob
+from os.path import join, splitext
+from shutil import rmtree
 import json
 import logging
 
@@ -41,6 +49,37 @@ def to_wkt():
     if geometry is None:
         return
     return geometry.ExportToWkt()
+
+@app.route('/geojson', methods=['POST'])
+def convert():
+    
+    zipped_shape = None
+    for k, v in request.files.to_dict().items():
+        if k.lower().endswith('.zip'):
+            zipped_shape = ZipFile(v)
+            break
+    if zipped_shape:
+        try:
+            extract_here = mkdtemp()
+            print(extract_here, flush=True)
+            zipped_shape.extractall(extract_here)
+            shape_files = glob(join(extract_here, '**', '*.shp'))
+            print(shape_files, flush=True)
+            features = []
+            json_path = join(extract_here, 'result.geojson')
+            options = gdal.VectorTranslateOptions(format="GeoJSON")
+
+            for shape_file in shape_files:
+                gdal.VectorTranslate(json_path, shape_file, options=options)
+            
+                with open(json_path, 'r') as jason:
+                    collection = json.load(jason)
+                features += collection['features']
+                
+            return {'type': 'FeatureCollection', 'features': features}
+        finally:
+            rmtree(extract_here)
+
 
 
 if __name__ == "__main__":
